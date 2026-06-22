bcd5e0c56f12316670e804f237d52e7f045d9a22
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-06-22 19:04:55 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,12 @@
+def Multiplication(No1, No2):
+    return No1*No2
+
+
+def main():
+    Ret = Multiplication(10,5)
+    print("Multiplication is:",Ret)
+
+    
+
+if __name__ == "__main__":
+    main()
